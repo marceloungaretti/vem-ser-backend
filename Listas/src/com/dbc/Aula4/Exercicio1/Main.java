6654cd25112767b26fc1d9b161775bdacd857036
marcelo <marcelo.ungaretti@gmail.com>
2021-10-08 15:43:50 -0300

    Exercicios da aula 4

---
+++ Listas/src/com/dbc/Aula4/Exercicio1/Main.java
@@ -0,0 +1,10 @@
+package com.dbc.Aula4.Exercicio1;
+
+public class Main {
+    public static void main(String[] args) {
+        Carro fiesta = new Carro();
+        Carro focus = new Carro();
+        Carro mercedes = new Carro("Classe A");
+        Carro ferrari = new Carro("Classe A", 122000);
+    }
+}
