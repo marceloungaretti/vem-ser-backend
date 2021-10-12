e2e9b0f04fa97fd5753760ef3bab4636c98cc8ad
Marcelo <marcelo.ungaretti@gmail.com>
2021-10-12 15:05:02 -0300

    Lista 2 - Exercício 4: Orientado à objetos Concluído; Lista 1 - Exercício 4: em andamento; Lista 2 - Exercício 3: em andamento;

---
+++ Listas/src/com/dbc/L1eL2OrientadasAObjetos/Lista2/Exercicio4/Main.java
@@ -0,0 +1,12 @@
+package com.dbc.L1eL2OrientadasAObjetos.Lista2.Exercicio4;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Digite 3 números: ");
+        Identificador posicao = new Identificador();
+        posicao.identificar();
+
+    }
+}
