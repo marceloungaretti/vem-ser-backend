7c78c64b87413695a71e9371fc1f36c94d80f046
Marcelo <marcelo.ungaretti@gmail.com>
2021-10-12 15:34:10 -0300

    Lista 2 - Concluída; Lista 1 - Exercício 4: revisar; Lista 2 - Exercício 3: revisar; Lista 2 - Exercício 8: revisar;

---
+++ Listas/src/com/dbc/L1eL2OrientadasAObjetos/Lista2/Exercicio8/Main.java
@@ -0,0 +1,10 @@
+package com.dbc.L1eL2OrientadasAObjetos.Lista2.Exercicio8;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Matriz a = new Matriz();
+        a.retornarMedias();
+    }
+}
