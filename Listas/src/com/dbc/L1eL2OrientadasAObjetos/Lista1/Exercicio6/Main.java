9379b66c8e1e24281e8a58beb676bf4862aa71b2
Marcelo <marcelo.ungaretti@gmail.com>
2021-10-12 13:14:39 -0300

    Lista 1 - Exercício 6: Orientado à objetos Concluído. Exercício 4 em andamento

---
+++ Listas/src/com/dbc/L1eL2OrientadasAObjetos/Lista1/Exercicio6/Main.java
@@ -0,0 +1,16 @@
+package com.dbc.L1eL2OrientadasAObjetos.Lista1.Exercicio6;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+        System.out.println("Insira a palavra a ser traduzida: ");
+        String palavra = scanner.nextLine();
+        Tradutor tradutor = new Tradutor(palavra);
+
+        tradutor.traduzir();
+    }
+}
