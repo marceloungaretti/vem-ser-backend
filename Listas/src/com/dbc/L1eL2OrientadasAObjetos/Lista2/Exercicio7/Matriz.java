92f31ce0a7233d4b871a9ef70febeff0be3e187d
Marcelo <marcelo.ungaretti@gmail.com>
2021-10-12 15:12:48 -0300

    Lista 2 - Exercícios 6 e 7: Orientado à objetos Concluído; Lista 1 - Exercício 4: em andamento; Lista 2 - Exercício 3: em andamento;

---
+++ Listas/src/com/dbc/L1eL2OrientadasAObjetos/Lista2/Exercicio7/Matriz.java
@@ -0,0 +1,29 @@
+package com.dbc.L1eL2OrientadasAObjetos.Lista2.Exercicio7;
+
+import java.util.Scanner;
+
+public class Matriz {
+
+    public void contagemMaiorQue10() {
+        Scanner scanner = new Scanner(System.in);
+        int[][] matriz = new int[4][4];
+        int contagem = 0;
+
+        for (int linha = 0; linha < 4; linha++) {
+            for (int coluna = 0; coluna < 4; coluna++) {
+                System.out.printf("%dª linha, %dª coluna = ", linha + 1, coluna + 1);
+                matriz[linha][coluna] = scanner.nextInt();
+            }
+        }
+
+        //contagem > 10
+        for (int linha = 0; linha < 4; linha++)
+            for (int coluna = 0; coluna < 4; coluna++)
+                if (matriz[linha][coluna] > 10)
+                    contagem++;
+
+
+        System.out.printf("\n\n Existem na matriz %d números maiores que 10.", contagem);
+
+    }
+}
