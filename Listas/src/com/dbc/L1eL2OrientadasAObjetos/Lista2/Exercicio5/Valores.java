5533840cc9ef1f3ab5fb1c3995bfe7e5d4a4d35f
Marcelo <marcelo.ungaretti@gmail.com>
2021-10-12 15:07:42 -0300

    Lista 2 - Exercício 5: Orientado à objetos Concluído; Lista 1 - Exercício 4: em andamento; Lista 2 - Exercício 3: em andamento;

---
+++ Listas/src/com/dbc/L1eL2OrientadasAObjetos/Lista2/Exercicio5/Valores.java
@@ -0,0 +1,20 @@
+package com.dbc.L1eL2OrientadasAObjetos.Lista2.Exercicio5;
+
+import java.util.Scanner;
+
+public class Valores {
+
+    public void escritaReversa() {
+        Scanner scanner = new Scanner(System.in);
+        int valor = 20;
+        int[] valores = new int[valor];
+
+        for (int i = 0; i < valor; i++) {
+            System.out.println("Digite um valor: ");
+            valores[i] = scanner.nextInt();
+        }
+        for (int i = valores.length; i > 0; i--) {
+            System.out.println(valores[i - 1]);
+        }
+    }
+}
