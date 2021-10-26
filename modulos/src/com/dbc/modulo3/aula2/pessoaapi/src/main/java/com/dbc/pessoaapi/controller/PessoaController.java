11114f0c57adc5251582160ffaf21f3eb65a63b0
Marcelo <marcelo.ungaretti@gmail.com>
2021-10-26 16:00:26 -0300

    projeto spring criado

---
+++ modulos/src/com/dbc/modulo3/aula2/pessoaapi/src/main/java/com/dbc/pessoaapi/controller/PessoaController.java
@@ -0,0 +1,12 @@
+package com.dbc.pessoaapi.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PessoaController {
+
+
+    public String hello() {
+        return "Hello World";
+    }
+}
